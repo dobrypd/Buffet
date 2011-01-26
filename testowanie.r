11dc5b8263d2cf5a8c363f6c8903e8ea28243308
Piotrek <dobrypd@gmail.com>
2011-01-26 01:57:05 +0100

    nothing2

--- testowanie.r
+++ testowanie.r
@@ -4,14 +4,19 @@
 
 #testowanie wykonania danej strategii
 #czyli policzenie zysku
-#DEBUG - TO CHWILOWO
-Gr = matrix(1:15, 5, 3);
-Gr[1,]  = c(3, 0.5, 0.5);
-Gr[2,]  = c(4, 0.4, 0.6);
-Gr[3,]  = c(5, 0.3, 0.7);
-Gr[4,]  = c(6, 0.2, 0.8);
-Gr[5,]  = c(7, 0.1, 0.9);
 
-test = function(M, k, Grupy) {
+generuj =  function(k){
+   Gr = matrix(1:15, 5, 3);
+   Gr[1,]  = c(3, 0.5, 0.5);
+   Gr[2,]  = c(4, 0.4, 0.6);
+   Gr[3,]  = c(5, 0.3, 0.7);
+   Gr[4,]  = c(6, 0.2, 0.8);
+   Gr[5,]  = c(7, 0.1, 0.9);
+   
+}
+test = function(M, k) {
+   #dla każdej grupy generuję ją 
+   #  
+   generuj(k)
    plot(Gr)
 }
