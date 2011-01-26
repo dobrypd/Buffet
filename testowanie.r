97df2b78bd8508941d53a03cce35643bf37e93d3
Piotrek <dobrypd@gmail.com>
2011-01-26 01:52:24 +0100

    nothing

--- testowanie.r
+++ testowanie.r
@@ -6,12 +6,12 @@
 #czyli policzenie zysku
 #DEBUG - TO CHWILOWO
 Gr = matrix(1:15, 5, 3);
-Grupy[1,]  = c(3, 0.5, 0.5);
-Grupy[2,]  = c(4, 0.4, 0.6);
-Grupy[3,]  = c(5, 0.3, 0.7);
-Grupy[4,]  = c(6, 0.2, 0.8);
-Grupy[5,]  = c(7, 0.1, 0.9);
+Gr[1,]  = c(3, 0.5, 0.5);
+Gr[2,]  = c(4, 0.4, 0.6);
+Gr[3,]  = c(5, 0.3, 0.7);
+Gr[4,]  = c(6, 0.2, 0.8);
+Gr[5,]  = c(7, 0.1, 0.9);
 
 test = function(M, k, Grupy) {
-   
+   plot(Gr)
 }
